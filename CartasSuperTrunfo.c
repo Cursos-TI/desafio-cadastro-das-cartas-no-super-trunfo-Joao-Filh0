71629c47baa3382ee2f56fc3e8360d834d270860
joao <joao.militao@magnumgroup.com.br>
2025-03-22 12:27:12 -0300

    Atualizado para Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,8 @@ int main() {
     float area1, area2, area3, area4;
     float pib1, pib2, pib3, pib4;
     int pontos_turisticos1, pontos_turisticos2, pontos_turisticos3, pontos_turisticos4;
-    
-    
+    float densidade_populacional1, densidade_populacional2, densidade_populacional3, densidade_populacional4;
+    float pib_per_capita1, pib_per_capita2, pib_per_capita3, pib_per_capita4;
 
     printf("\nCadastro da cidade 1:\n\n\n");
     printf("Código da cidade (ex: A01): ");
@@ -20,7 +20,7 @@ int main() {
     scanf("%f", &pib1);
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos1);
-    
+
     printf("\nCadastro da cidade 2:\n");
     printf("Código da cidade (ex: A02): ");
     scanf("%s", codigo2);
@@ -32,7 +32,7 @@ int main() {
     scanf("%f", &pib2);
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos2);
-    
+
     printf("\nCadastro da cidade 3:\n");
     printf("Código da cidade (ex: B01): ");
     scanf("%s", codigo3);
@@ -44,7 +44,7 @@ int main() {
     scanf("%f", &pib3);
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos3);
-    
+
     printf("\nCadastro da cidade 4:\n");
     printf("Código da cidade (ex: B02): ");
     scanf("%s", codigo4);
@@ -56,16 +56,36 @@ int main() {
     scanf("%f", &pib4);
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos4);
-    
+
+
+    densidade_populacional1 = populacao1 / area1;
+    pib_per_capita1 = pib1 / populacao1;
+
+    densidade_populacional2 = populacao2 / area2;
+    pib_per_capita2 = pib2 / populacao2;
+
+    densidade_populacional3 = populacao3 / area3;
+    pib_per_capita3 = pib3 / populacao3;
+
+    densidade_populacional4 = populacao4 / area4;
+    pib_per_capita4 = pib4 / populacao4;
+
     printf("\nCidades cadastradas:\n");
     printf("##### - CIDADE 1 - #####");
-    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n\n", codigo1, populacao1, area1, pib1, pontos_turisticos1);
+    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f bilhões\n\n",
+           codigo1, populacao1, area1, pib1, pontos_turisticos1, densidade_populacional1, pib_per_capita1);
+
     printf("##### - CIDADE 2 - #####");
-    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n\n", codigo2, populacao2, area2, pib2, pontos_turisticos2);
+    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f bilhões\n\n",
+           codigo2, populacao2, area2, pib2, pontos_turisticos2, densidade_populacional2, pib_per_capita2);
+
     printf("##### - CIDADE 3 - #####");
-    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n\n", codigo3, populacao3, area3, pib3, pontos_turisticos3);
+    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f bilhões\n\n",
+           codigo3, populacao3, area3, pib3, pontos_turisticos3, densidade_populacional3, pib_per_capita3);
+
     printf("##### - CIDADE 4 - #####");
-    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n\n", codigo4, populacao4, area4, pib4, pontos_turisticos4);
-    
+    printf("\nCódigo: %s\nPopulação: %d habitantes\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f bilhões\n\n",
+           codigo4, populacao4, area4, pib4, pontos_turisticos4, densidade_populacional4, pib_per_capita4);
+
     return 0;
 }
